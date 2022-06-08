1992932c83a458033d7827ad6ae29c67b0c380f2
zoelbathosai <84974857+zoelbathosai@users.noreply.github.com>
2022-06-08 09:15:52 +0700

    2st update

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ Sederhana Menggunakan Streamlit
 """)
 alas = st.number_input("Masukkan nilai alas",0)
 tinggi = st.number_input("Masukkan nilai tinggi",0)
+hitung = st.button ("Hitung Luas")
+
+if hitung :
+  luas = 0.5*alas*tinggi
+  st.write("Luas Segitiga nya =",luas)
